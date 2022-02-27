fce1b6f916b442bfc75b886785680620de7a80c7
Вадим Желтов <99135277+ZhVadim@users.noreply.github.com>
2022-02-28 00:35:30 +0300

    Желтов В.А. 21-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,70 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t kol = 0;
+    for (uint64_t i = 2; i <= sqrt(value); i++) {
+        if (value % i == 0) {
+            kol += 1;
+        }
+    }
+    if (kol == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    return 0;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t prost = 0;
+    uint64_t ch = 0;
+    for (uint64_t p = 2; ch < n; p++) {
+        uint64_t kol = 0;
+        for (uint64_t i = 2; i <= sqrt(p); i++) {
+            if (p % i == 0) {
+                kol += 1;
+            }
+        }
+        if (kol == 0) {
+            prost = p;
+            ch += 1;
+        }
+    }
+    return prost;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  uint64_t prost = 0;
+    uint64_t ch = 0;
+    for (uint64_t p = value + 1; ch < 1; p++) {
+        uint64_t kol = 0;
+        for (uint64_t i = 2; i <= sqrt(p); i++) {
+            if (p % i == 0) {
+                kol += 1;
+            }
+        }
+        if (kol == 0) {
+            prost = p;
+            ch += 1;
+        }
+    }
+    return prost;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+    uint64_t ch = 0;
+    for (uint64_t p = 2; p < hbound; p++) {
+        uint64_t kol = 0;
+        for (uint64_t i = 2; i <= sqrt(p); i++) {
+            if (p % i == 0) {
+                kol += 1;
+            }
+        }
+        if (kol == 0) {
+            sum += p;
+        }
+    }
+    return sum;
 }
